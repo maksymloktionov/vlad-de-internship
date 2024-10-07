e6dfe1586f090222ff69a1004f848c5837c3bd3b
Влад <user@MacBook-Pro.local>
2024-10-07 19:32:08 +0300

    Add docker environment and upgrade script to OOP concepts

--- _python/site_parser.py
+++ _python/site_parser.py
@@ -3,66 +3,93 @@ import bs4
 import pandas as pd
 import requests
 
-def website_to_df(): 
-    url = "https://uk.wikipedia.org/wiki/2000"
-    content = requests.request("get", url)
-    soup = bs4.BeautifulSoup(content.text, 'html.parser')
+class Webscrapper:
 
-    all_lists = soup.find_all('ul', id="",recursive=True)
-    res = []
+    def __init__(self, url: str) -> None:
+        self.url = url
 
-    for ele in all_lists:
-        if not ele.get('class') and len(ele) == 21:
-            text_list = ele.text.replace("\xa0", "").split("\n")
+    def __start_connections(self) -> ps.extensions.connection:
+        try:
+            connection = ps.connect(
+                host = "db",
+                port = "5432",
+                dbname = "postgres",
+                password = "1234",
+                user = "postgres"
+            )
 
-    for line in text_list:
-        splitted = line.split("— ")
-        splitter = splitted[1].index(",")
+            return connection
+        except Exception as e:
+            print(f"Error: {e}")
 
-        res.append((splitted[0], splitted[1][:splitter], splitted[1][splitter+2:]))
+    def __get_data(self) -> list[tuple[str, str, str]]:
+        content = requests.request("get", self.url)
+        soup = bs4.BeautifulSoup(content.text, "html.parser")
 
-    dict_data = {
-        "Date": [ele[0] + ' 2000' for ele in res],
-        "Name": [ele[1] for ele in res],
-        "Info": [ele[2] for ele in res],
-    }
+        all_lists = soup.find_all('ul', id="", recursive=True)
+        res = []
 
-    df = pd.DataFrame.from_dict(dict_data)
+        for ele in all_lists:
+            if not ele.get('class') and len(ele) == 21:
+               text_list = ele.text.replace("\xa0", "").split("\n")
 
-    return df
+        for line in text_list:
+            splitted = line.split("— ")
+            splitter = splitted[1].index(",")
 
+            res.append((splitted[0], splitted[1][:splitter], splitted[1][splitter+2:]))
 
-df = website_to_df()
-
-try:
-    connection = ps.connect(
-        host = "localhost",
-        port = "5444",
-        dbname = "postgres",
-        password = "1234",
-        user = "postgres"
-    )
-
-    cursor = connection.cursor()
-
-    cursor.execute("""CREATE TABLE IF NOT EXISTS public.Sites ( 
-                        id INT PRIMARY KEY,
-                        date VARCHAR(32),
-                        name VARCHAR(50),
-                        content TEXT
-                   )""")
+        return res
     
-    connection.commit()
-    
-    for i, row in df.iterrows():
-        cursor.execute("""
-            INSERT INTO public.Sites (id, date, name, content) 
-            VALUES (%s, %s, %s, %s) 
-            ON CONFLICT (id) DO NOTHING
-            """, (i, row['Date'], row['Name'], row['Info']))
-        connection.commit()
+    def __construct_df(self) -> pd.DataFrame:
+        site_data = self.__get_data()
 
-except Exception as e:
-    print(e)
+        dict_data = {
+            "Date": [ele[0] + ' 2000' for ele in site_data],
+            "Name": [ele[1] for ele in site_data],
+            "Info": [ele[2] for ele in site_data],
+        }
+
+        df = pd.DataFrame.from_dict(dict_data)
+
+        return df
+    
+    def website_data_to_db(self) -> None:
+        try:
+            connection = self.__start_connections()
+            df = self.__construct_df()
+
+            cursor = connection.cursor()
+
+            cursor.execute("""CREATE TABLE IF NOT EXISTS public.Sites ( 
+                    id INT PRIMARY KEY,
+                    date VARCHAR(32),
+                    name VARCHAR(50),
+                    content TEXT
+               )""")
+    
+            connection.commit()
+
+            for i, row in df.iterrows():
+                cursor.execute("""
+                    INSERT INTO public.Sites (id, date, name, content) 
+                    VALUES (%s, %s, %s, %s) 
+                    ON CONFLICT (id) DO NOTHING
+                    """, (i, row['Date'], row['Name'], row['Info']))
+                connection.commit()
+
+            print("Done")
+
+            cursor.close()
+            connection.close()
+            
+        except Exception as e:
+            print(f"Error: {e}")
+
+if __name__ == "__main__":
+
+    webscrp = Webscrapper("https://uk.wikipedia.org/wiki/2000")
+
+    webscrp.website_data_to_db()
 
 
---
+++ _python/start_script.sh
@@ -0,0 +1,3 @@
+docker build -t webscrapper .
+
+docker compose up
