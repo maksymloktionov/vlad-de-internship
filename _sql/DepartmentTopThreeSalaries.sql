c33ecb14523f939b7083c851eae16db3aed60beb
Влад <user@MacBook-Pro.local>
2024-10-10 15:42:55 +0300

    Update code to cover SAME NAME AND DPRT case

--- _sql/DepartmentTopThreeSalaries.sql
+++ _sql/DepartmentTopThreeSalaries.sql
@@ -1,16 +1,22 @@
 -- Department Top Three Salaries (https://leetcode.com/problems/department-top-three-salaries/description/)
 
-SELECT dprt.name AS Department, empl.name AS employee, s.salary
-FROM Employee AS empl
-INNER JOIN Department AS dprt
-ON empl.departmentId = dprt.id
-INNER JOIN (
-    SELECT name as new_name, salary
-    FROM
-    (
-    SELECT name, salary, DENSE_RANK() OVER(PARTITION BY departmentId ORDER BY salary DESC) as rank
-    FROM Employee
-    )
+WITH EmplDeps AS (
+    SELECT dprt.name AS Department,
+           empl.name as Employee,
+           empl.salary AS Salary,
+           DENSE_RANK() OVER(
+            PARTITION BY departmentId ORDER BY salary DESC
+            ) AS rank
+    FROM Employee as empl
+    INNER JOIN Department as dprt
+    ON empl.departmentId = dprt.id
+    ORDER BY Department, Salary DESC
+),
+SortedSalaries AS (
+    SELECT Department, Employee, Salary
+    FROM EmplDeps
     WHERE rank <= 3
-) AS slr
-ON empl.name = slr.new_name AND empl.salary = slr.salary
+)
+
+SELECT *
+FROM SortedSalaries
