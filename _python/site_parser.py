8832d12066c1101e4fb2275b5bc3b88be4392986
Влад <user@MacBook-Pro.local>
2024-10-17 14:25:50 +0300

    Update site_parser.py; add gitignore

--- _python/site_parser.py
+++ _python/site_parser.py
@@ -4,92 +4,76 @@ import pandas as pd
 import requests
 import os
 import logging
-from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
-load_dotenv()
 
 class Webscrapper:
 
     def __init__(self, url: str) -> None:
+        logging.info("Script started")
         self.url = url
+
 
     @staticmethod
     def _start_connections() -> ps.extensions.connection:
         try:
-            connection = ps.connect(
-                host = os.getenv('DB_HOST'),
-                port = os.getenv('DB_PORT'),
-                dbname = os.getenv('DB_NAME'),
-                password = os.getenv('PASSWORD'),
-                user = os.getenv('DB_USER')
-            )
-
-            return connection
+            engine = create_engine(f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}")
+            return engine
         except Exception as e:
-            logging.info(f"Error: {e}")
+            logging.exception(f"Error: {e}")
 
     @staticmethod
     def _get_data(url: str) -> list[tuple[str, str, str]]:
-        content = requests.request("get", url)
-        soup = bs4.BeautifulSoup(content.text, "html.parser")
+        try:
+            content = requests.request("get", url)
+            soup = bs4.BeautifulSoup(content.text, "html.parser")
 
-        all_lists = soup.find_all('ul', id="", recursive=True)
-        res = []
+            all_lists = soup.find_all('ul', id="", recursive=True)
+            res = []
 
-        for ele in all_lists:
-            if not ele.get('class') and len(ele) == 21:
-               text_list = ele.text.replace("\xa0", "").split("\n")
+            for ele in all_lists:
+                if not ele.get('class') and len(ele) == 21:
+                    text_list = ele.text.replace("\xa0", "").split("\n")
 
-        for line in text_list:
-            splitted = line.split("— ")
-            splitter = splitted[1].index(",")
+            for line in text_list:
+                splitted = line.split("— ")
+                splitter = splitted[1].index(",")
 
-            res.append((splitted[0], splitted[1][:splitter], splitted[1][splitter+2:]))
+                res.append((splitted[0], splitted[1][:splitter], splitted[1][splitter+2:]))
 
-        return res
+            logging.info(f"Data has been succesfully parsed from {url}")
+
+            return res
+        except Exception as e:
+            logging.exception(f"During execution {e} ocurred")
     
     def _construct_df(self) -> pd.DataFrame:
-        site_data = self._get_data(self.url)
+        try:
+            site_data = self._get_data(self.url)
 
-        dict_data = {
-            "Date": [f"{date} 2000" for date, _, _ in site_data],
-            "Name": [name for _, name, _ in site_data],
-            "Info": [info for _, _, info in site_data],
-        }
+            dict_data = {
+                "Date": [f"{date} 2000" for date, _, _ in site_data],
+                "Name": [name for _, name, _ in site_data],
+                "Info": [info for _, _, info in site_data],
+            }
 
-        df = pd.DataFrame.from_dict(dict_data)
+            df = pd.DataFrame.from_dict(dict_data)
 
-        return df
+            logging.info("Data has been succesfully moved to DataFrame")
+
+            return df
+        except Exception as e:
+            logging.exception(f"During execution {e} ocurred")
     
     def website_data_to_db(self) -> None:
         try:
-            connection = self._start_connections()
             df = self._construct_df()
+            engine = self._start_connections()
 
-            cursor = connection.cursor()
+            df.to_sql('Sites', engine, if_exists='replace', index_label='id')
 
-            cursor.execute("""CREATE TABLE IF NOT EXISTS public.Sites ( 
-                    id INT PRIMARY KEY,
-                    date VARCHAR(32),
-                    name VARCHAR(50),
-                    content TEXT
-               )""")
-    
-            connection.commit()
-
-            for i, row in df.iterrows():
-                cursor.execute("""
-                    INSERT INTO public.Sites (id, date, name, content) 
-                    VALUES (%s, %s, %s, %s) 
-                    ON CONFLICT (id) DO NOTHING
-                    """, (i, row['Date'], row['Name'], row['Info']))
-                connection.commit()
-
-            logging.info("Data has been successfully moved")
-
-            cursor.close()
-            connection.close()
+            logging.info("Data has been successfully loaded")
             
         except Exception as e:
             logging.info(f"Error: {e}")
